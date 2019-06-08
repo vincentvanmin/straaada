dc8defb8f8a7069da5ed3775b72eeebd758c35ca
vincentvanmin <vincentvm98@hotmail.com>
2019-06-09 00:59:13 +0200

    loading animation

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -46,6 +46,11 @@
                                     <div class="plane"></div>
                                 </button>
                             </div>
+                            <div id="spinner" class="spinner">
+                                <div class="bounce1"></div>
+                                <div class="bounce2"></div>
+                                <div class="bounce3"></div>
+                            </div>
                         </form>
                     </mdb-col>
                 </mdb-row>
@@ -89,6 +94,7 @@ export default {
         },
 
         handleSubmit() {
+            document.getElementById('spinner').style.opacity="1";
             fetch('/', {
                 method: 'post',
                 headers: {
@@ -311,6 +317,50 @@ export default {
                 }
             }
         } 
+
+        #spinner {
+            opacity: 0;
+        }
+
+        .spinner {
+            margin: 20px auto 0 auto;
+            width: 70px;
+            text-align: center;
+
+            > div {
+                width: 18px;
+                height: 18px;
+                background-color: $action-color;
+
+                border-radius: 100%;
+                display: inline-block;
+                -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+                animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+            }
+
+            .bounce1 {
+                -webkit-animation-delay: -0.32s;
+                animation-delay: -0.32s;
+            }
+            .bounce2 {
+                -webkit-animation-delay: -0.16s;
+                animation-delay: -0.16s;
+            }
+
+            @-webkit-keyframes sk-bouncedelay {
+                0%, 80%, 100% { -webkit-transform: scale(0) }
+                40% { -webkit-transform: scale(1.0) }
+            }
+            @keyframes sk-bouncedelay {
+                0%, 80%, 100% { 
+                    -webkit-transform: scale(0);
+                    transform: scale(0);
+                } 40% { 
+                    -webkit-transform: scale(1.0);
+                    transform: scale(1.0);
+                }
+            }
+        }
     }
 }
 </style>
